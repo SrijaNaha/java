5b2ca35bd5558d98c1ad30c4ad4026c9b0fa0309
SrijaNaha <nahasrija17@gmail.com>
2025-07-10 02:19:07 +0530

    Create Program7.java

---
+++ Assignment-3/Program7.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Assignment3
+{
+public static void main(String [] args)
+{
+for(int i=1;i<100;i++)
+{
+if(i%2!=0)
+{
+System.out.print(i + " ");
+}
+}
+System.out.print("100");
+}
+}
