9a7c667343d32e1ddc233578b52fa4030130cc3d
SrijaNaha <nahasrija17@gmail.com>
2025-07-10 02:17:16 +0530

    Create Program4.java

---
+++ Assignment-3/Program4.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Assignment3 {
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+
+       
+        System.out.print("Enter a number: ");
+        int number = ob.nextInt();
+
+        
+        boolean isPrime = true;
+
+        if (number <= 1) 
+        {
+        isPrime = false;
+        } 
+         else {
+       
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+                if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+}
