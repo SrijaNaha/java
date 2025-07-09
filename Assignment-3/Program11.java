29327f6df51e959b85292d5a5f077d60e9888e7e
SrijaNaha <nahasrija17@gmail.com>
2025-07-10 02:23:13 +0530

    Create Program11.java

---
+++ Assignment-3/Program11.java
@@ -0,0 +1,24 @@
+public class Assignment3
+ {
+public static void main(String[] args) 
+{
+        
+String red = "\u001B[31m";
+String reset = "\u001B[0m";
+for (int i = 1; i <= 5; i++) 
+{
+for (int j = 1; j <= i; j++) 
+{
+if(i!=1 && j==1)
+{
+System.out.print(red + "* " + reset);
+}
+else
+{
+System.out.print("* ");
+}
+}
+System.out.println();
+}
+}
+}
