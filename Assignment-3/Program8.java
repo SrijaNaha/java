f7fdb9e6be076715356b6b08b26d343e653e2f40
SrijaNaha <nahasrija17@gmail.com>
2025-07-10 02:20:16 +0530

    Create Program8.java

---
+++ Assignment-3/Program8.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Assignment3
+{
+public static void main(String [] args)
+{
+int i = 1;
+int j = 2;
+while(i<=100)
+{
+System.out.print(i + " ");
+i = i + j;
+j = j + 2;
+}
+}
+}
